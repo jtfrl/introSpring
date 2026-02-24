a8a9ce0cdb1077d9d28a4f001ae54bc3a56b1400
Jefferson Tafarel <jeffersontafarel.jor@outlook.com>
2026-02-24 17:01:40 -0300

    changes on generatorServ

--- Qrcode/src/main/java/com/ajax/Qrcode/service/QrCodeGeneratorService.java
+++ Qrcode/src/main/java/com/ajax/Qrcode/service/QrCodeGeneratorService.java
@@ -1,9 +1,11 @@
 package com.ajax;
+package com.ajax.Qrcode.service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream;
 
 import org.springframework.stereotype.Service;
 import com.google.zxing.BarcodeFormat;
@@ -11,6 +13,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
+
 
 @Service
 public class QrCodeGeneratorService {
